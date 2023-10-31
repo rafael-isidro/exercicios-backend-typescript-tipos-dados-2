9dd5a1f129afc19fd819635a07c883c6077e9762
rafael-isidro <rafa-isidro@hotmail.com>
2023-10-31 01:29:44 -0300

    Exercício 01 - Módulo 03 - Finalizado

---
+++ 01/index.ts
@@ -0,0 +1,17 @@
+import { readFileSync, writeFileSync } from "fs";
+const filePath = "./bd.json";
+
+const fileReader = (fileName: string): unknown => {
+  return JSON.parse(readFileSync(fileName, "utf-8"));
+};
+
+const fileWritter = (fileName: any, content: any): void => {
+  writeFileSync(fileName, JSON.stringify(content));
+};
+
+const data = fileReader(filePath) as string[];
+
+data.push("Aí?");
+fileWritter(filePath, data);
+
+console.log(fileReader(filePath));
