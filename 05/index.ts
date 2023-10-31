a0c7b1a2bfe60a3ca8458a5784dae38e0ca3b288
rafael-isidro <rafa-isidro@hotmail.com>
2023-10-31 15:58:37 -0300

    Exercício 05 - Módulo 03 - Finalizado

---
+++ 05/index.ts
@@ -0,0 +1,93 @@
+import { existsSync, readFileSync, writeFileSync } from "fs";
+
+type Adress = {
+  cep: string;
+  rua: string;
+  complemento?: string;
+  bairro: string;
+  cidade: string;
+};
+
+type User = {
+  nome: string;
+  email: string;
+  cpf: string;
+  profissao?: string;
+  endereco?: Adress | null;
+};
+
+const userExample = {
+  nome: "Arodo Sobrenome",
+  email: "Exemplo2@email.com",
+  cpf: "002.444.333-00",
+  profissao: "Desenvolvedor Backend",
+  endereco: {
+    cep: "100002-000",
+    rua: "B",
+    bairro: "Liberdade",
+    cidade: "São Paulo",
+  },
+};
+
+const registerUser = (user: User): User => {
+  let bd: User[] = [];
+  if (!user.endereco) user.endereco = null;
+  if (existsSync("./bd.json")) {
+    bd = getUsers();
+  }
+
+  bd.push(user);
+  writeFileSync("./bd.json", JSON.stringify(bd));
+  return user;
+};
+
+const detailUser = (cpf: string): User => {
+  const bd = fileReader() as User[];
+  const userFound = bd.find((user) => user.cpf === cpf);
+  if (!userFound) {
+    throw new Error("User not found");
+  }
+  return userFound;
+};
+
+const updateUser = (cpf: string, data: User): User => {
+  const bd = fileReader() as User[];
+  const userFound = bd.find((user) => user.cpf === cpf);
+  if (!userFound) {
+    throw new Error("User not found");
+  }
+
+  Object.assign(userFound, data);
+  writeFileSync("./bd.json", JSON.stringify(bd));
+  return userFound;
+};
+
+const deleteUser = (cpf: string): User => {
+  const bd = fileReader() as User[];
+  const userFound = bd.find((user) => user.cpf === cpf);
+  if (!userFound) {
+    throw new Error("User not found");
+  }
+  bd.splice(bd.indexOf(userFound), 1);
+
+  writeFileSync("./bd.json", JSON.stringify(bd));
+  return userFound;
+};
+
+const getUsers = (profissao?: string): User[] => {
+  const bd = fileReader() as User[];
+
+  const users = bd.filter((user) => user.profissao === profissao);
+  if (users.length < 1) {
+    throw new Error("User not found");
+  }
+  return users;
+};
+
+function fileReader(): unknown {
+  if (existsSync("./bd.json"))
+    return JSON.parse(readFileSync("./bd.json", "utf-8"));
+  else return [];
+}
+
+getUsers("Médico");
